b87a150505b7c1694c4de07ff6a1508ff49ac13a
DL <ge45roh@mytum.de>
2025-09-27 09:29:28 +0530

    switched from btc dApp to ethereum dApp for 1inch sdk

--- contract/js/futureTest.js
+++ contract/js/futureTest.js
@@ -21,28 +21,43 @@ const oracleAbi = JSON.parse(
 const escrow = new ethers.Contract(process.env.ESCROW_ADDRESS, escrowAbi, wallet);
 const oracle = new ethers.Contract(process.env.ORACLE_ADDRESS, oracleAbi, wallet);
 
+// provide expiry in days
+const create_future = async (expiry) => {
+  const [, price] = await oracle.latestRoundData();
+  // 2. Define notional = $1
+  const notional = 1e8; // $1 with 8 decimals
+
+  const expiry_time = Math.floor(Date.now() / 1000) + 24 * 60 * 60;
+
+  // TODO add proper margin calculation
+  const marginTotal = ethers.parseUnits("0.00002", 18); // 2x 0.00001
+
+  return {
+    notional : notional,
+    expiry : expiry_time,
+    margin : marginTotal,
+  }
+}
+
 async function main() {
   // 1. Read gold price (entry price)
   const [, price] = await oracle.latestRoundData();
   console.log("Gold price:", Number(price) / 1e8, "USD");
 
-  // 2. Define notional = $1
-  const notional = 1e8; // $1 with 8 decimals
+  // expiry in days
+  const {notional, expiry, margin} = await create_future(1);
 
-  // 3. Expiry = 1 day
-  const expiry = Math.floor(Date.now() / 1000) + 24 * 60 * 60;
 
-  // 4. Both parties margin in cBTC (say 0.00001 each)
-  const marginTotal = ethers.parseUnits("0.00002", 18); // 2x 0.00001
 
   // 5. Call matchAndEscrow
+  // TODO implement limit order protocol
   const tx = await escrow.matchAndEscrow(
     "0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC",  // taker address
     0,                     // Side.LONG (maker is long)
     notional,
     expiry,
     price,
-    { value: marginTotal }
+    { value: margin }
   );
   await tx.wait();
   console.log("Future opened for $1 notional GOLD");
